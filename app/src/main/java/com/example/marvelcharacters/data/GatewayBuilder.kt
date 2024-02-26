e9d021e437b9665d7ec67ac0cd9f0e24694ca314
danielle-andrade <dannielle_andrade@hotmail.com>
2024-02-26 13:37:16 -0300

    builder

--- app/src/main/java/com/example/marvelcharacters/data/GatewayBuilder.kt
+++ app/src/main/java/com/example/marvelcharacters/data/GatewayBuilder.kt
@@ -7,30 +7,30 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 
-object GatewayBuilder {
-
-    private const val BASE_URL = "https://gateway.marvel.com/"
-    private val contentType = "application/json".toMediaType()
-
-    @OptIn(ExperimentalSerializationApi::class)
-    private val json = Json {
-        ignoreUnknownKeys = true
-        explicitNulls = false
-    }
-
-    private val client = OkHttpClient.Builder()
-        .addInterceptor(MarvelInterceptor())
-        .build()
-
-    @OptIn(ExperimentalSerializationApi::class)
-    val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(client)
-            .addConverterFactory(json.asConverterFactory(contentType))
-            .build()
-    }
-
-    inline fun <reified T> build(): T =
-        retrofit.create(T::class.java)
-}
+//object GatewayBuilder {
+//
+//    private const val BASE_URL = "https://gateway.marvel.com/"
+//    private val contentType = "application/json".toMediaType()
+//
+//    @OptIn(ExperimentalSerializationApi::class)
+//    private val json = Json {
+//        ignoreUnknownKeys = true
+//        explicitNulls = false
+//    }
+//
+//    private val client = OkHttpClient.Builder()
+//        .addInterceptor(MarvelInterceptor())
+//        .build()
+//
+//    @OptIn(ExperimentalSerializationApi::class)
+//    val retrofit: Retrofit by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .client(client)
+//            .addConverterFactory(json.asConverterFactory(contentType))
+//            .build()
+//    }
+//
+//    inline fun <reified T> build(): T =
+//        retrofit.create(T::class.java)
+//}
